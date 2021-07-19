5d879a2e91ae516c467c04de55d3db632397cf2b
Abhishek Pawar <13kyunaie@gmail.com>
2021-07-19 23:51:49 +0530

    katie now takes text commands

--- katie/baclup.py
+++ katie/baclup.py
@@ -13,7 +13,6 @@ voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
 
 def takeCommand():
-    speak("How can I help you?")
     r = sr.Recognizer()
     with sr.Microphone() as source:
         print("Listening...")
@@ -27,7 +26,7 @@ def takeCommand():
 
     except Exception as e:
         # print(e)    
-        speak("Say that again please...")
+        print("Say that again please...")
         return "None"
     return query
 
@@ -40,25 +39,18 @@ def speak(audio):
 def wishme():
     hour = int(datetime.datetime.now().hour)
     if hour >=0 and hour < 12:
-        speak("Good Morning Abhy!")
+        speak("Good Morning Abhy! I am Katie, How can I help you?")
     elif hour >12 and hour < 18:
-        speak("Good Afternoon Abhy!")
+        speak("Good Afternoon Abhy! I am Katie, How can I help you?")
     else:
-        speak("Good Evening Abhy!")
+        speak("Good Evening Abhy! I am Katie, How can I help you?")
 
 
 def main():
+    print('in main')
     wishme()
-    args = sys.argv[1:]
-    str = ''
-    for arg in args:
-        # print (arg)
-        # print(ar)
-        str = str + " " + arg
-    # print(str)
-    # exit()
-    if(args):
-        query = str
+    while True:
+        query = takeCommand().lower()
         if 'wikipedia' in query:
             speak('Searching Wikipedia...')
             query = query.replace("wikipedia", "")
@@ -87,38 +79,5 @@ def main():
         elif 'stop' in query:
             speak("GoodBye!")
             exit()
-
-
-    else:
-        while True:
-            query = takeCommand().lower()
-            if 'wikipedia' in query:
-                speak('Searching Wikipedia...')
-                query = query.replace("wikipedia", "")
-                results = wikipedia.summary(query, sentences=2) 
-                speak("According to Wikipedia")
-
-                speak(results)
-            elif 'open youtube' in query:
-                webbrowser.open("youtube.com")
-            elif 'open google' in query:
-                webbrowser.open("google.com")
-            elif 'open twitter' in query:
-                webbrowser.open("twitter.com")
-            elif 'open spotify' in query:
-                codePath = "C:\\Users\\Star\\AppData\\Roaming\\Spotify\\Spotify.exe"
-                os.startfile(codePath)
-
-            elif 'open code' in query:
-                codePath = "C:\\Users\\Star\\AppData\\Local\\Programs\\Microsoft VS Code\Code.exe"
-                os.startfile(codePath)
-            elif 'how are you' in query:
-                speak("I am fine, Abhy! How are you?")
-            elif 'quit' in query:
-                speak("GoodBye!")
-                exit()
-            elif 'stop' in query:
-                speak("GoodBye!")
-                exit()
 if __name__ == '__main__':
     main()
